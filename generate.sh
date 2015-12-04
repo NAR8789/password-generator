1fb7b1562d1552b9a2f73f6f3b52c938791d9416
xiao.fan <xiao.fan@workday.com>
2015-12-04 09:18:11 -0800

    try gshuf if shuf is unavailable

--- generate.sh
+++ generate.sh
@@ -3,4 +3,11 @@
 LENGTH="$1"
 LENGTH="${LENGTH:=4}"
 
-shuf -n"$LENGTH" --random-source=/dev/urandom
+command -v gshuf >/dev/null 2>&1 && SHUF=gshuf
+command -v shuf  >/dev/null 2>&1 && SHUF=shuf
+if [ -z "$SHUF" ]; then
+  >&2 echo "could not find shuf or gshuf"
+  exit 1
+fi
+
+"$SHUF" -n "$LENGTH" --random-source=/dev/urandom
