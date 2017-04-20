7d5cf3de5cd08d514db4b018716b2333f03b7b7e
xiao.fan <xiao.fan@workday.com>
2017-04-19 18:08:13 -0700

    fix quotes

--- generate.sh
+++ generate.sh
@@ -6,7 +6,7 @@ PICK="${PICK:=5}"
 command -v gshuf >/dev/null 2>&1 && SHUF=gshuf
 command -v shuf  >/dev/null 2>&1 && SHUF=shuf
 if [ -z "$SHUF" ]; then
-  >&2 echo "could not find shuf or gshuf"
+  >&2 echo 'could not find shuf or gshuf'
   exit 1
 fi
 
